4c1d7677d39dcc63e407ec0074c787afd58f5ba3
Bing <hongbing0419@gmail.com>
2022-10-11 13:45:39 +0800

    combine

---
+++ ft_hi.c/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thong-bi <thong-bi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 18:40:19 by thong-bi          #+#    #+#             */
+/*   Updated: 2022/10/06 18:40:19 by thong-bi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	char	*tmp;
+
+	i = 0;
+	tmp = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!tmp || s == NULL || f == NULL)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		tmp[i] = f(i, s[i]);
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
