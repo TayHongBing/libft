3e36599e12b57210ee6006d02e797335b8c397a4
Bing <hongbing0419@gmail.com>
2022-10-12 16:18:54 +0800

    bonus1

---
+++ ft_hi.c/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thong-bi <thong-bi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 15:49:42 by thong-bi          #+#    #+#             */
+/*   Updated: 2022/10/12 15:49:42 by thong-bi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new.next = NULL;
+	new.content = content;
+	return (new);
+}
